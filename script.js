5765a38bb2cbf3e9bc78eb617371cd55bd957f76
jacod <jakubcoding@gmail.com>
2022-11-28 21:27:22 +0100

    fixed background on main page

--- script.js
+++ script.js
@@ -2,17 +2,21 @@ const toggle = document.querySelector('#darkmode-toggle');
 const mainImage = document.querySelector('.mainRightImg');
 
 function dark_mode(){
-    if(toggle.checked === true){
+    if(toggle.checked){
         document.body.style.background = "url('png/dark_background.png')";
-        document.body.style.transition = "0.3s";
-        mainImage.src = "png/homePageMainImageWhite.png";
+        document.body.style.backgroundRepeat = "no-repeat";
+        document.body.style.transition = "0.3s ease-in-out";
+        document.body.style.backgroundColor = "rgb(47,47,47)";
         mainImage.style.transition = "0.3s";
+        mainImage.src = "png/homePageMainImageWhite.png";
         
     } else {
         document.body.style.background = "url('png/blue_background.png')";
-        document.body.style.transition = "0.3s";
-        mainImage.src = "png/homePageMainImage.png";
+        document.body.style.backgroundRepeat = "no-repeat";
+        document.body.style.transition = "0.3s ease-in-out";
+        document.body.style.backgroundColor = "rgb(51,99,231)";
         mainImage.style.transition = "0.3s";
+        mainImage.src = "png/homePageMainImage.png";
     }
 }
 
