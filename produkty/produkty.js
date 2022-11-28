2809e95d4ede0db966f35cf8ccc6ef2b16f5bd4d
jacod <jakubcoding@gmail.com>
2022-11-28 18:20:40 +0100

    added products

--- produkty/produkty.js
+++ produkty/produkty.js
@@ -1,6 +1,19 @@
 const products = [
-{
-      id: 0,
+    {
+      id: 3,
+      name: "iPhone 14 Pro Max",
+      description: `Ekran: 6,7"<br>
+      Procesor: Apple A16 Bionic<br>
+      Pamięć RAM: 6 GB<br>
+      Pamięć wbudowana: 1 TB`,
+      category: "apple",
+      price: 10499,
+      image: "produkty png/iphone14.png",
+      sale: true,
+      saleAmount: 100,
+    },
+    {
+    id: 0,
       name: "iPhone 11",
       description: `Ekran: 6,1"<br>
       Procesor: Apple A13 Bionic<br>
@@ -34,7 +47,20 @@ const products = [
     },
     {
       id: 3,
-      name: "huawei p50 pro",
+      name: "Xiaomi Redmi 10C",
+      description: `Ekran: 6,71"<br>
+      Procesor: Qualcomm Snapdragon 680<br>
+      Pamięć RAM: 4 GB<br>
+      Pamięć wbudowana: 64 GB`,
+      category: "xiaomi",
+      price: 730,
+      image: "produkty png/xiaomi-redmi-10c.png",
+      sale: true,
+      saleAmount: 100,
+    },
+    {
+      id: 3,
+      name: "Huawei P50 Pro",
       description: `Ekran: 6,6"<br>
       Procesor: Qualcomm Snapdragon 888<br>
       Pamięć RAM: 8 GB<br>
@@ -45,7 +71,7 @@ const products = [
     },
     {
       id: 4,
-      name: "huawei p20 lite",
+      name: "Huawei P20 Lite",
       description: `Ekran: 6,6"<br>
       Procesor: Qualcomm Snapdragon 888<br>
       Pamięć RAM: 8 GB<br>
@@ -56,41 +82,31 @@ const products = [
     },
     {
       id: 5,
-      name: "Nokia c300",
-      description: `Procesor Zegar procesora: 208 MHz<br>
-      Pamięć RAM 64 MB<br>
-      Pamięć wbudowana: 55 MB`,
-      category: "nokia",
-      price: 650,
-      image: "produkty png/Nokia.jpg",
+      name: "Samsung Galaxy Z Fold4",
+      description: `Ekran: 6,2", 7,6" - ekran po rozłożeniu<br>
+      Procesor: Qualcomm Snapdragon 8<br>
+      Pamięć RAM: 12 GB<br>
+      Pamięć wbudowana: 512 GB`,
+      category: "samsung",
+      price: 8999,
+      image: "produkty png/samsung-galaxy-z-fold4.png",
       sale: true,
-      saleAmount: 250,
+      saleAmount: 1000,
     },
     {
-      id: 6,
-      name: "Nokia n70",
-      description: `Ekran: 2,8"<br>
-      Procesor: Spreadtrum SC6531F<br>
-      Pamięć RAM: 8 MB<br>
-      Pamięć wbudowana: 16 MB`,
-      category: "nokia",
-      price: 690,
-      image: "produkty png/Nokia-2.jpg",
-    },
-    {
-      id: 7,
-      name: "Samsung a53",
+      id: 5,
+      name: "Samsung A53",
       description: `Ekran: 2,8"<br>
       Procesor: Spreadtrum SC6531F<br>
       Pamięć RAM: 8 MB<br>
       Pamięć wbudowana: 16 MB`,
       category: "samsung",
-      price: 1440,
+      price: 1440.44,
       image: "produkty png/Samsung-a53.jpg",
     },
     {
-      id: 8,
-      name: "Samsung s20",
+      id: 6,
+      name: "Samsung S20",
       description: `Ekran: 6,5"<br>
       Procesor: Qualcomm Snapdragon 865<br>
       Pamięć RAM: 8 GB<br>
@@ -102,51 +118,14 @@ const products = [
       saleAmount: 300,
     },
     {
-      id: 9,
-      name: "Samsung s22",
+      id: 7,
+      name: "Samsung S22",
       description: `Ekran: 6,6"<br>
       Procesor: Samsung Exynos 2200<br>
       Pamięć RAM: 8 GB<br>
       Pamięć wbudowana: 128 GB`,
       category: "samsung",
-      price: 3399.99,
+      price: 3399.5,
       image: "produkty png/Samsung-s22.jpg",
-    },
-    {
-      id: 10,
-      name: "iPhone 11",
-      description: `Ekran: 6,1"<br>
-      Procesor: Apple A14 Bionic<br>
-      Pamięć RAM: 4 GB<br>
-      Pamięć wbudowana: 128 GB`,
-      category: "apple",
-      price: 3999.99,
-      image: "produkty png/iphone-11.jpg",
-      sale: true,
-      saleAmount: 200,
-    },
-    {
-      id: 11,
-      name: "iPhone 13",
-      description: `Ekran: 6,1"<br>
-      Procesor: Apple A15 Bionic<br>
-      Pamięć RAM: 4 GB<br>
-      Pamięć wbudowana: 128 GB`,
-      category: "apple",
-      price: 4999.5,
-      image: "produkty png/iphone-13.jpg",
-    },
-    {
-      id: 12,
-      name: "iPhone 12",
-      description: `Ekran: 6,1"<br>
-      Procesor: Apple A14 Bionic<br>
-      Pamięć RAM: 4 GB<br>
-      Pamięć wbudowana: 128 GB`,
-      category: "apple",
-      price: 4444.44,
-      image: "produkty png/iPhone-12.jpg",
-      sale: true,
-      saleAmount: 451,
     },
   ];
