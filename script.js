33bd938649397528294ac4683f7a5afa5db884cc
jacod <jakubcoding@gmail.com>
2022-11-29 18:36:47 +0100

    updated background on main page

--- script.js
+++ script.js
@@ -1,21 +1,35 @@
 const toggle = document.querySelector('#darkmode-toggle');
 const mainImage = document.querySelector('.mainRightImg');
+const bottomWaveColor = document.querySelectorAll('.bottom-wave .shape-fill');
+const upperWaveColor = document.querySelectorAll('.upper-wave .shape-fill')
 
 function dark_mode(){
     if(toggle.checked){
-        document.body.style.background = "url('png/dark_background.png')";
-        document.body.style.backgroundRepeat = "no-repeat";
+        bottomWaveColor.forEach(element => {
+            element.style.transition = "all .3s ease-in-out";
+            element.style.fill = "#242424";
+        });
+        upperWaveColor.forEach(element => {
+            element.style.transition = "all .3s ease-in-out";
+            element.style.fill = "#242424";
+        });
         document.body.style.transition = "0.3s ease-in-out";
         document.body.style.backgroundColor = "rgb(47,47,47)";
-        mainImage.style.transition = "0.3s";
+        mainImage.style.transition = "all 0.3s ease-in-out";
         mainImage.src = "png/homePageMainImageWhite.png";
         
     } else {
-        document.body.style.background = "url('png/blue_background.png')";
-        document.body.style.backgroundRepeat = "no-repeat";
+        bottomWaveColor.forEach(element => {
+            element.style.transition = "all .3s ease-in-out";
+            element.style.fill = "#264ACA";
+        })
+        upperWaveColor.forEach(element => {
+            element.style.transition = "all .3s ease-in-out";
+            element.style.fill = "#264ACA";
+        })
         document.body.style.transition = "0.3s ease-in-out";
         document.body.style.backgroundColor = "rgb(51,99,231)";
-        mainImage.style.transition = "0.3s";
+        mainImage.style.transition = "all 0.3s ease-in-out";
         mainImage.src = "png/homePageMainImage.png";
     }
 }
