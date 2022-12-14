5eeb9f33cdfcdf7089d80a8f94746708d0bb59e3
jacod <jakubcoding@gmail.com>
2022-12-15 00:39:59 +0100

    bug fixes + localStorage

--- script.js
+++ script.js
@@ -1,41 +1,60 @@
 const toggle = document.querySelector("#darkmode-toggle");
-const mainImage = document.querySelector(".mainRightImg");
+const mainImage = document.querySelector(".main-right-img");
 const bottomWaveColor = document.querySelectorAll(".bottom-wave .shape-fill");
 const upperWaveColor = document.querySelectorAll(".upper-wave .shape-fill");
 
-function dark_mode() {
+function darkModeHandler() {
   if (toggle.checked) {
-    bottomWaveColor.forEach((element) => {
-      element.style.transition = "all .3s ease-in-out";
-      element.style.fill = "#242424";
-    });
-    upperWaveColor.forEach((element) => {
-      element.style.transition = "all .3s ease-in-out";
-      element.style.fill = "#242424";
-    });
-    document.body.style.transition = "0.3s ease-in-out";
-    document.body.style.backgroundColor = "rgb(47,47,47)";
-    mainImage.style.transition = "all 0.3s ease-in-out";
-    mainImage.src = "png/homePageMainImageWhite.png";
+    localStorage.setItem("theme", "dark");
+    dark();
   } else {
-    bottomWaveColor.forEach((element) => {
-      element.style.transition = "all .3s ease-in-out";
-      element.style.fill = "#264ACA";
-    });
-    upperWaveColor.forEach((element) => {
-      element.style.transition = "all .3s ease-in-out";
-      element.style.fill = "#264ACA";
-    });
-    document.body.style.transition = "0.3s ease-in-out";
-    document.body.style.backgroundColor = "rgb(51,99,231)";
-    mainImage.style.transition = "all 0.3s ease-in-out";
-    mainImage.src = "png/homePageMainImage.png";
+    localStorage.setItem("theme", "light");
+    light();
   }
 }
 
-const responsiveList = document.querySelector(".responsiveList");
-const mobileHeaderLinks = document.querySelector("#headerLinks");
-const mobileHeaderButtons = document.querySelector("#headerButtons");
+let theme = localStorage.getItem("theme");
+if (theme === "dark") {
+  dark();
+} else if (theme === "light") {
+  light();
+}
+
+function dark() {
+  toggle.checked = true;
+  bottomWaveColor.forEach((element) => {
+    element.style.transition = "all .3s ease-in-out";
+    element.style.fill = "#242424";
+  });
+  upperWaveColor.forEach((element) => {
+    element.style.transition = "all .3s ease-in-out";
+    element.style.fill = "#242424";
+  });
+  document.body.style.transition = "0.3s ease-in-out";
+  document.body.style.backgroundColor = "rgb(47,47,47)";
+  mainImage.style.transition = "all 0.3s ease-in-out";
+  mainImage.src = "png/homePageMainImageWhite.png";
+}
+
+function light() {
+  toggle.checked = false;
+  bottomWaveColor.forEach((element) => {
+    element.style.transition = "all .3s ease-in-out";
+    element.style.fill = "#264ACA";
+  });
+  upperWaveColor.forEach((element) => {
+    element.style.transition = "all .3s ease-in-out";
+    element.style.fill = "#264ACA";
+  });
+  document.body.style.transition = "0.3s ease-in-out";
+  document.body.style.backgroundColor = "rgb(51,99,231)";
+  mainImage.style.transition = "all 0.3s ease-in-out";
+  mainImage.src = "png/homePageMainImage.png";
+}
+
+const responsiveList = document.querySelector(".responsive-list");
+const mobileHeaderLinks = document.querySelector("#header-links");
+const mobileHeaderButtons = document.querySelector("#header-buttons");
 
 responsiveList.addEventListener("click", function () {
   mobileHeaderLinks.classList.toggle("active");
