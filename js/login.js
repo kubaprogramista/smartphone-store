b6b457f6478c7dba19c56d1134be85d915c5f29b
jacod <jakubcoding@gmail.com>
2022-12-18 02:04:08 +0100

    Update login.js

--- js/login.js
+++ js/login.js
@@ -2,6 +2,7 @@ const toggle = document.querySelector("#darkmode-toggle");
 const mainImage = document.querySelector(".main-right-img");
 const bottomWaveColor = document.querySelectorAll(".bottom-wave .shape-fill");
 const upperWaveColor = document.querySelectorAll(".upper-wave .shape-fill");
+const loginSection = document.querySelector(".login-section");
 
 function darkModeHandler() {
   if (toggle.checked) {
@@ -30,8 +31,12 @@ function dark() {
     element.style.transition = "all .3s ease-in-out";
     element.style.fill = "#242424";
   });
-  document.body.style.transition = "0.3s ease-in-out";
+  document.body.style.transition = "background 0.3s ease-in-out";
   document.body.style.backgroundColor = "rgb(47,47,47)";
+
+  loginSection.style.transition = "background 0.3s ease-in-out";
+  loginSection.style.background =
+    "linear-gradient(rgba(29, 29, 29, 1), rgba(40, 40, 40, 1))";
 }
 
 function light() {
@@ -46,6 +51,10 @@ function light() {
   });
   document.body.style.transition = "0.3s ease-in-out";
   document.body.style.backgroundColor = "rgb(51,99,231)";
+  // --darkBlue: rgba(38, 74, 202, 1);
+  // --lightBlue: rgba(51, 99, 231, 1);
+  loginSection.style.background =
+    "linear-gradient(rgba(255,255,255, .1), rgba(255,255,255, .05))";
 }
 
 const responsiveList = document.querySelector(".responsive-list");
