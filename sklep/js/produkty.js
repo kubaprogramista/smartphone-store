c8883411af0edff3c413dc9221aed358a08dc33e
jacod <jakubcoding@gmail.com>
2022-12-18 08:38:30 +0100

    Update produkty.js

--- sklep/js/produkty.js
+++ sklep/js/produkty.js
@@ -1,120 +1,120 @@
 const products = [
-    {
-      id: 0,
-      name: "iPhone 14 Pro Max",
-      description: `Ekran: 6,7"<br>
+  {
+    id: 0,
+    name: "iPhone 14 Pro Max",
+    description: `Ekran: 6,7"<br>
       Procesor: Apple A16 Bionic<br>
       Pamięć RAM: 6 GB<br>
       Pamięć wbudowana: 1 TB`,
-      category: "apple",
-      price: 10499,
-      image: "produkty png/iphone14.png",
-      sale: true,
-      saleAmount: 100,
-    },
-    {
+    category: "apple",
+    price: 10499,
+    image: "produkty png/iphone14.png",
+    sale: true,
+    saleAmount: 100,
+  },
+  {
     id: 1,
-      name: "iPhone 11",
-      description: `Ekran: 6,1"<br>
+    name: "iPhone 11",
+    description: `Ekran: 6,1"<br>
       Procesor: Apple A13 Bionic<br>
       Pamięć RAM: 4 GB<br>
       Pamięć wbudowana: 128 GB`,
-      category: "apple",
-      price: 2999.99,
-      image: "produkty png/iphone11.png",
-    },
-    {
-      id: 2,
-      name: "iPhone 13",
-      description: `Ekran: 6,1"<br>
+    category: "apple",
+    price: 2999.99,
+    image: "produkty png/iphone11.png",
+  },
+  {
+    id: 2,
+    name: "iPhone 13",
+    description: `Ekran: 6,1"<br>
       Procesor: Apple A15 Bionic<br>
       Pamięć RAM: 4 GB<br>
       Pamięć wbudowana: 512 GB`,
-      category: "apple",
-      price: 6599.99,
-      image: "produkty png/iphone13.png",
-    },
-    {
-      id: 3,
-      name: "iPhone 12",
-      description: `Ekran: 6,1"<br>
+    category: "apple",
+    price: 6599.99,
+    image: "produkty png/iphone13.png",
+  },
+  {
+    id: 3,
+    name: "iPhone 12",
+    description: `Ekran: 6,1"<br>
       Procesor: Apple A14 Bionic<br>
       Pamięć RAM: 4 GB<br>
       Pamięć wbudowana: 128 GB`,
-      category: "apple",
-      price: 3999.99,
-      image: "produkty png/iphone12.png",
-    },
-    {
-      id: 4,
-      name: "Xiaomi 12 Pro",
-      description: `Ekran: 6,73"<br>
+    category: "apple",
+    price: 3999.99,
+    image: "produkty png/iphone12.png",
+  },
+  {
+    id: 4,
+    name: "Xiaomi 12 Pro",
+    description: `Ekran: 6,73"<br>
       Procesor: Qualcomm Snapdragon 8<br>
       Pamięć RAM: 12 GB<br>
       Pamięć wbudowana: 256 GB`,
-      category: "xiaomi",
-      price: 4999,
-      image: "produkty png/xiaomi-12-pro.png",
-      sale: true,
-      saleAmount: 799,
-    },
-    {
-      id: 5,
-      name: "Xiaomi Redmi 10C",
-      description: `Ekran: 6,71"<br>
+    category: "xiaomi",
+    price: 4999,
+    image: "produkty png/xiaomi-12-pro.png",
+    sale: true,
+    saleAmount: 799,
+  },
+  {
+    id: 5,
+    name: "Xiaomi Redmi 10C",
+    description: `Ekran: 6,71"<br>
       Procesor: Qualcomm Snapdragon 680<br>
       Pamięć RAM: 4 GB<br>
       Pamięć wbudowana: 64 GB`,
-      category: "xiaomi",
-      price: 730,
-      image: "produkty png/xiaomi-redmi-10c.png",
-      sale: true,
-      saleAmount: 100,
-    },
-    {
-      id: 6,
-      name: "Samsung Z Fold4",
-      description: `Ekran: 6,2", 7,6" - ekran po rozłożeniu<br>
+    category: "xiaomi",
+    price: 730,
+    image: "produkty png/xiaomi-redmi-10c.png",
+    sale: true,
+    saleAmount: 100,
+  },
+  {
+    id: 6,
+    name: "Samsung Z Fold4",
+    description: `Ekran: 6,2", 7,6" - ekran po rozłożeniu<br>
       Procesor: Qualcomm Snapdragon 8<br>
       Pamięć RAM: 12 GB<br>
       Pamięć wbudowana: 512 GB`,
-      category: "samsung",
-      price: 8999,
-      image: "produkty png/samsung-galaxy-z-fold4.png",
-    },
-    {
-      id: 7,
-      name: "Samsung S22 Ultra",
-      description: `Ekran: 6,8"<br>
+    category: "samsung",
+    price: 8999,
+    image: "produkty png/samsung-galaxy-z-fold4.png",
+  },
+  {
+    id: 7,
+    name: "Samsung S22 Ultra",
+    description: `Ekran: 6,8"<br>
       Procesor: Samsung Exynos 2200<br>
       Pamięć RAM: 12 GB<br>
       Pamięć wbudowana: 512 GB`,
-      category: "samsung",
-      price: 6899.99,
-      image: "produkty png/samsung-s22-ultra.png",
-      sale: true,
-      saleAmount: 900,
-    },
-    {
-      id: 8,
-      name: "Huawei P50 Pro",
-      description: `Ekran: 6,6"<br>
+    category: "samsung",
+    price: 6899.99,
+    image: "produkty png/samsung-s22-ultra.png",
+    sale: true,
+    saleAmount: 900,
+  },
+  {
+    id: 8,
+    name: "Huawei P50 Pro",
+    description: `Ekran: 6,6"<br>
       Procesor: Qualcomm Snapdragon 888<br>
       Pamięć RAM: 8 GB<br>
       Pamięć wbudowana: 256 GB`,
-      category: "huawei",
-      price: 4499,
-      image: "produkty png/huawei-1.jpg",
-    },
-    {
-      id: 9,
-      name: "Huawei Nova Y90",
-      description: `Ekran: 6,7"<br>
+    category: "huawei",
+    price: 4499,
+    image: "produkty png/huawei-1.jpg",
+  },
+  {
+    id: 9,
+    name: "Huawei Nova Y90",
+    description: `Ekran: 6,7"<br>
       Procesor: Qualcomm Snapdragon 680<br>
       Pamięć RAM: 6 GB<br>
       Pamięć wbudowana: 128 GB`,
-      category: "huawei",
-      price: 1099,
-      image: "produkty png/huawei-2.jpg",
-    },
-  ];
+    category: "huawei",
+    price: 1099,
+    image: "produkty png/huawei-2.jpg",
+  },
+];
